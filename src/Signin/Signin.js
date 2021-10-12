6a2cbb3753fc58403ffdf37f1a63ad624a82b40f
Jean-François <stilgar182@gmail.com>
2021-10-12 15:07:43 +0200

    repouet

--- src/Signin/Signin.js
+++ src/Signin/Signin.js
@@ -4,21 +4,15 @@ import { useState } from "react";
 
 function Signin() {
 
-  const [pseudo, setPseudo] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [pseudo, setPseudo] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   
   const handleSubmit = async (e) => {
     e.preventDefault();
     let submit = { pseudo, email, password }
 
-    // Fonctionnement de la fonction Fetch, ouais c'est comme ça, apparement faut pas chercher
-    // c'est un peu comme la fonction map, c'est comme ça, puis c'est comme ça.
-    // déjà 'fetch' en lui même je le trouve très mauvais comme mot pour notre cas !
-    // elle t'indique donc OU du dois fetcher, la methode, ce qu'elle doit envoyer et le headers,
-    // bha c'est comme ça, faut pas chercher.
-
-    return fetch('http://localhost:5000/contact', {
+    return fetch('http://localhost:5000/users', {
       method: 'POST',
       body: JSON.stringify(submit),
       headers: {
@@ -39,18 +33,18 @@ function Signin() {
     <Form className="signInBox">
       <Form.Group className="mb-3" >
         <Form.Label>Pseudo</Form.Label>
-        <Form.Control className="test" type="text" placeholder="Choisis ton pseudo" onInput={(e) => handleInput(e, setPseudo)} />
+        <Form.Control className="test" type="text" placeholder="Choisis ton pseudo" name="pseudo" onInput={(e) => handleInput(e, setPseudo)} />
       </Form.Group>
       <Form.Group className="mb-3" >
         <Form.Label>Adresse email</Form.Label>
-        <Form.Control type="email" placeholder="Email" onInput={(e) => handleInput(e, setEmail)} />
+        <Form.Control type="email" placeholder="Email" name="email" onInput={(e) => handleInput(e, setEmail)} />
         <Form.Text className="text-muted">
           On garde ton email pour nous t'inquiètes!
         </Form.Text>
       </Form.Group>
       <Form.Group className="mb-3" >
         <Form.Label>Mot de passe</Form.Label>
-        <Form.Control type="password" placeholder="Mot de passe" onInput={(e) => handleInput(e, setPassword)} />
+        <Form.Control type="password" placeholder="Mot de passe" name="password" onInput={(e) => handleInput(e, setPassword)} />
       </Form.Group>
       <Button className="boutonSignin" type="submit" onClick={() => handleSubmit()}>
         Crée ton compte
