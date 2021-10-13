ac58b5693b8e4090ac8c29e5f6c59bfcd382d13b
Jean-François <stilgar182@gmail.com>
2021-10-13 14:50:51 +0200

    \o/

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,19 +1,46 @@
 import "./Login.css";
 import { Button, Form } from 'react-bootstrap';
+import { useState } from "react";
+
 
 function Login() {
+
+  // Nos petites constantes d'état qui vont bient
+  const [pseudo, setPseudo] = useState();
+  const [password, setPassword] = useState();
+
+const handleClick = async() => {
+
+  let submit = await {pseudo, password}
+
+  return fetch('http://localhost:5000/users/login' ,
+
+
+  { method: 'GET',
+    body: JSON.stringify(submit),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+  .then(data => console.log(data));
+}
+
+  // LA SEXYYY HOT FUNCTION
+  function handleInput(e, setter) {
+    setter(e.target.value)
+  }
 
   return (
     <Form className="loginBox">
       <Form.Group className="mb-3" >
         <Form.Label><span className="labelLog">Pseudo</span></Form.Label>
-        <Form.Control type="text" placeholder="Entre ton pseudo" />
+        <Form.Control type="text" placeholder="Entre ton pseudo" onInput={(e) => handleInput(e, setPseudo)} />
       </Form.Group>
       <Form.Group className="mb-3" >
         <Form.Label><span className="labelLog">Mot de passe</span></Form.Label>
-        <Form.Control type="password" placeholder="Mot de passe" />
+        <Form.Control type="password" placeholder="Mot de passe" onInput={(e) => handleInput(e, setPassword)} />
       </Form.Group>
-      <Button className="boutonLogin">
+      <Button className="boutonLogin" onClick={() => handleClick()}>
         Valider
       </Button>
     </Form>
--- src/Signin/Signin.js
+++ src/Signin/Signin.js
@@ -4,27 +4,40 @@ import { useState } from "react";
 
 function Signin() {
 
+  // Constates d'état des differents inputs 
   const [pseudo, setPseudo] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [verifPassword, setverifPassword] = useState();
 
-  const handleSubmit = async (e) => {
-    let submit = await { pseudo, email, password, verifPassword }
-    return fetch('http://localhost:5000/users'
-      , {
-        method: 'POST',
-        body: JSON.stringify(submit),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
+  const handleClick = async (e) => {
 
+    // récupére les inputs dans le body
+
+    let submit = { pseudo, email, password, verifPassword }
+
+    // FONCTION FETCH 
+    // ça t'envoie sur la route que tu veux utiliser. 
+    // Le fetch il ya plein de propriétés differentes
+    // le minimum syndical pour qu'il fonctionne c'est la method, le stringify parce qu'il envoie sur un json et headers.
+    // le Hearders, faut pas se poser de questions, c'est juste comme ça.
+
+    return fetch(
+      'http://localhost:5000/users'
+    ,{
+      method: 'POST',
+      body: JSON.stringify(submit),
+      headers: {
+        'Content-Type': 'application/json'
+    } } )
+        // ces lignes c'est comme le headers, c'est comme ça et puis c'est tout
       .then(res => res.json())
       .then(data => console.log(data));
   }
 
   //Fonction Callback sur les inputs du formulaire
+  // sexy version 
+
   function handleInput(e, setter) {
     setter(e.target.value)
   }
@@ -50,7 +63,7 @@ function Signin() {
         <Form.Label><span className="labelSign"> Veuillez verifier votre mot de passe</span></Form.Label>
         <Form.Control type="password" placeholder="Confirmer le mot de passe" name="verifPassword" onInput={(e) => handleInput(e, setverifPassword)} />
       </Form.Group>
-      <Button className="boutonSignin" onClick={() => handleSubmit()}>
+      <Button className="boutonSignin" onClick={() => handleClick()}>
         Crée ton compte
       </Button>
     </Form>
